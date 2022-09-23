e5d1c9a3a9af24d7296039cc8e7715211c6e71fe
Michael Frolov <moyshedev@gmail.com>
2022-09-23 15:23:22 +0300

    Create new app 'debts' and 'clients'

--- settings/settings.py
+++ settings/settings.py
@@ -33,6 +33,8 @@ ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 ROOT_APPS = [
     'apps.core',
+    'apps.clients',
+    'apps.debts',
 ]
 
 INSTALLED_APPS = [
@@ -79,22 +81,22 @@ WSGI_APPLICATION = 'settings.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'localhost',
-        'PORT': '5432'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'postgres',
+#         'USER': 'postgres',
+#         'PASSWORD': 'postgres',
+#         'HOST': 'localhost',
+#         'PORT': '5432'
+#     }
+# }
 
 
 # Password validation
