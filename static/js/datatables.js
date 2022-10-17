83e510e9f9b761b77797889909df5f2345e72bff
Michael Frolov <moyshedev@gmail.com>
2022-10-17 17:06:44 +0300

    The java script for displaying the table has been exported to a separate file 'static/js/datatable.js'

--- static/js/datatables.js
+++ static/js/datatables.js
@@ -4,7 +4,7 @@ $("#example").DataTable({
     pageLength: 20, // Data per page
     lengthChange: true, // Show entries per page
     autoWidth: true, // Control the auto width on columns
-    searching: false, // Input search panel
+    searching: true, // Input search panel
     bInfo: false, // Show "How muth rows in this table" in footer
     bSort: true, // Sorted A to Z and Z to A(and numbers)
 });
